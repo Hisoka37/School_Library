617847db73060998dce5de23e97921990b1f57bc
Walid Kabboussa <walidkabou@gmail.com>
2023-05-22 23:04:59 +0100

    Fix liters

--- person.rb
+++ person.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # Person section
 class Person
   attr_reader :id
--- student.rb
+++ student.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require_relative 'person'
 
 # Student section
@@ -12,6 +10,6 @@ class Student < Person
   end
 
   def play_hooky
-    "¯\(ツ)/¯"
+    '¯(ツ)/¯'
   end
 end
