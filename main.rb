e85ee77910b7a577bda63b12857ea24555ba66a6
Walid Kabboussa <walidkabou@gmail.com>
2023-05-26 19:38:50 +0100

    Fix linters

--- main.rb
+++ main.rb
@@ -1,42 +1,72 @@
 require './app'
 
 class Main
+  MENU_OPTIONS = {
+    '1' => :list_all_books,
+    '2' => :list_all_people,
+    '3' => :create_person,
+    '4' => :create_book,
+    '5' => :create_rental,
+    '6' => :list_rentals,
+    '7' => :exit
+  }.freeze
+
   def initialize
     @app = App.new
   end
 
   def menu
     puts 'Welcome to the School Library App:'
+
     loop do
-      puts 'Please choose an option by entering a number:'
-      puts '1 - List all books'
-      puts '2 - List all people'
-      puts '3 - Create a person'
-      puts '4 - Create a book'
-      puts '5 - Create a rental'
-      puts '6 - List all rentals for a given person id'
-      puts '7 - Exit'
+      display_menu_options
       option = gets.chomp
 
-      case option
-      when '1'
-        @app.list_all_books
-      when '2'
-        @app.list_all_people
-      when '3'
-        @app.create_person
-      when '4'
-        @app.create_book
-      when '5'
-        @app.create_rental
-      when '6'
-        @app.list_rentals
-      when '7'
-        puts 'Thank you for using our library ¯\^-^/¯'
-        return
+      if valid_option?(option)
+        execute_option(option)
       else
         puts 'Please enter a number between 1 and 7'
       end
+    end
+  end
+
+  private
+
+  def display_menu_options
+    puts 'Please choose an option by entering a number:'
+    MENU_OPTIONS.each { |key, value| puts "#{key} - #{option_description(value)}" }
+  end
+
+  def option_description(option)
+    case option
+    when :list_all_books
+      'List all books'
+    when :list_all_people
+      'List all people'
+    when :create_person
+      'Create a person'
+    when :create_book
+      'Create a book'
+    when :create_rental
+      'Create a rental'
+    when :list_rentals
+      'List all rentals for a given person id'
+    when :exit
+      'Exit'
+    end
+  end
+
+  def valid_option?(option)
+    MENU_OPTIONS.keys.include?(option)
+  end
+
+  def execute_option(option)
+    case MENU_OPTIONS[option]
+    when :exit
+      puts 'Thank you for using our library ¯\^-^/¯'
+      exit
+    else
+      @app.send(MENU_OPTIONS[option])
     end
   end
 end
